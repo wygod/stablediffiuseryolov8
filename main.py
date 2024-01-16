eb411bd0b0776ba510e756650b5d2b219ee80b10
wygod <2667946747@qq.com>
2024-01-16 10:17:51 +0800

    add code

--- main.py
+++ main.py
@@ -1,16 +1,12 @@
-# This is a sample Python script.
+# -*- encoding:utf-8-*-
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+from kosmos_generate import kosmos_generate
+from owlv2_generate import owlv2_generate
+from stable_diffuser_self import diffuser_v2_sdxl
 
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+
+
+if __name__ == "__main__":
+    pass
